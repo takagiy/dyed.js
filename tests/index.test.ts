492b2b84b332ea120b5046d95df4a6023adb7278
takagiy <takagiy.4dev@gmail.com>
2024-06-30 17:47:41 +0900

    add unit tests

---
+++ tests/index.test.ts
@@ -0,0 +1,22 @@
+import test from "ava";
+import { DiContainer } from "../src";
+
+type Injected = {
+  foo: string;
+};
+
+test("call getter outside of beginContext", (t) => {
+  const di = DiContainer.create<Injected>();
+  t.throws(() => di.foo);
+});
+
+test("inject value", (t) => {
+  const di = DiContainer.create<Injected>();
+  di.beginContext({ foo: "foo" }, () => {
+    t.is(di.foo, "foo");
+  });
+  di.beginContext({ foo: "bar" }, () => {
+    t.is(di.foo, "bar");
+  });
+  t.throws(() => di.foo);
+});
