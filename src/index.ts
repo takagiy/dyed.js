d19d2b278b036093e05f434401a915fd46c2432d
takagiy <takagiy.4dev@gmail.com>
2024-06-30 18:21:21 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,38 @@
 import { AsyncLocalStorage } from "node:async_hooks";
 
 type KeyValueOf<T> = {
-  [K in Extract<keyof T, string>]: T[K];
+  [K in Extract<keyof T, string | symbol>]: T[K];
 };
 
-type ValueOf<T> = T[Extract<keyof T, string>];
+type ValueOf<T> = T[Extract<keyof T, string | symbol>];
 
 export class DiContainer<T extends object> {
   private readonly context = new AsyncLocalStorage<T>();
 
-  private constructor() {
-    // biome-ignore lint/correctness/noConstructorReturn:
-    return new Proxy(this, {
-      get: (self: DiContainer<T>, propertyName: string) => {
-        return propertyName in self
-          ? self[propertyName as keyof typeof self]
-          : this.get(propertyName);
-      },
-    });
-  }
+  private constructor() {}
 
   public static create<T extends object>(): DiContainer<T> & KeyValueOf<T> {
-    return new DiContainer<T>() as DiContainer<T> & KeyValueOf<T>;
+    return new Proxy(new DiContainer<T>(), {
+      get: (self, propertyName) => {
+        return propertyName in self
+          ? self[propertyName as keyof typeof self]
+          : self.get(propertyName);
+      },
+    }) as DiContainer<T> & KeyValueOf<T>;
   }
 
   public beginContext<R, F extends () => R>(injected: T, callback: F): R {
     return this.context.run(injected, callback);
   }
 
-  private get(propertyName: string): ValueOf<T> | undefined {
+  private get(propertyName: string | symbol): ValueOf<T> | undefined {
     const injected = this.context.getStore();
     if (injected === undefined) {
       throw new Error(
         `DiContainer.get(${JSON.stringify(propertyName)}) called outside of DiContainer.beginContext().`,
       );
     }
-    return (injected as { [key: string]: ValueOf<T> | undefined })[
+    return (injected as { [key: string | symbol]: ValueOf<T> | undefined })[
       propertyName
     ];
   }
